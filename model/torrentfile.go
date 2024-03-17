e7b00439748493f74b9b53dc694e5f7012172514
Matthew Orford <mattorford9@gmail.com>
2024-03-17 11:48:16 -0400

    format and improve test coverage

--- model/torrentfile.go
+++ model/torrentfile.go
@@ -8,32 +8,32 @@ import (
 )
 
 type TorrentInfo struct {
-    Pieces      string `bencode:"pieces"`
-    PieceLength int    `bencode:"piece length"`
-    Length      int    `bencode:"length"`
-    Name        string `bencode:"name"`
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
 }
 
 type TorrentFile struct {
-    Announce string      `bencode:"announce"`
-    Info     TorrentInfo `bencode:"info"`
+	Announce string      `bencode:"announce"`
+	Info     TorrentInfo `bencode:"info"`
 }
 
 func Open(path string) (*TorrentFile, error) {
-    if path == "" {
-        return nil, errors.New("path cannot be empty")
-    }
+	if path == "" {
+		return nil, errors.New("path cannot be empty")
+	}
 
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    torrentFile := TorrentFile{}
-    err = bencode.Unmarshal(file, &torrentFile)
-    if err != nil {
-        return nil, err
-    }
-    return &torrentFile, nil
+	torrentFile := TorrentFile{}
+	err = bencode.Unmarshal(file, &torrentFile)
+	if err != nil {
+		return nil, err
+	}
+	return &torrentFile, nil
 }
--- model/torrentfile_test.go
+++ model/torrentfile_test.go
@@ -1,22 +1,58 @@
 package torrentfile
 
 import (
+	"errors"
+	"io"
+	"os"
 	"testing"
 )
 
-func TestOpen(t *testing.T) {
-	torrentFile, err := Open("testdata/archlinux-2019.12.01-x86_64.iso.torrent")
-    if err != nil {
-        t.Fatalf("Open failed: %v", err)
-    }
-		
-    expectedAnnounce := "http://tracker.archlinux.org:6969/announce"
-    if torrentFile.Announce != expectedAnnounce {
-        t.Errorf("Unexpected Announce value: got %q, want %q", torrentFile.Announce, expectedAnnounce)
-    }
+type MockFile struct {
+	data []byte
+}
 
-    expectedComment := "archlinux-2019.12.01-x86_64.iso"
-    if torrentFile.Info.Name != expectedComment {
-        t.Errorf("Unexpected Comment value: got %q, want %q", torrentFile.Info.Name, expectedComment)
-    }
+// Read reads data from the mock file.
+func (f *MockFile) Read(p []byte) (n int, err error) {
+	return copy(p, f.data), io.EOF
+}
+
+var bencodeUnmarshal = func(r io.Reader, v interface{}) error {
+	return nil // Default behavior: return no error
+}
+
+func TestOpen(t *testing.T) {
+	t.Run("ValidFile", func(t *testing.T) {
+		torrentFile, err := Open("testdata/archlinux-2019.12.01-x86_64.iso.torrent")
+		if err != nil {
+			t.Fatalf("Open failed: %v", err)
+		}
+
+		expectedAnnounce := "http://tracker.archlinux.org:6969/announce"
+		if torrentFile.Announce != expectedAnnounce {
+			t.Errorf("Unexpected Announce value: got %q, want %q", torrentFile.Announce, expectedAnnounce)
+		}
+
+		expectedComment := "archlinux-2019.12.01-x86_64.iso"
+		if torrentFile.Info.Name != expectedComment {
+			t.Errorf("Unexpected Comment value: got %q, want %q", torrentFile.Info.Name, expectedComment)
+		}
+	})
+
+	t.Run("EmptyPath", func(t *testing.T) {
+		_, err := Open("")
+		if err == nil {
+			t.Error("Expected error, got nil")
+		} else if err.Error() != "path cannot be empty" {
+			t.Errorf("Unexpected error message: got %q, want %q", err.Error(), "path cannot be empty")
+		}
+	})
+
+	t.Run("InvalidFile", func(t *testing.T) {
+		_, err := Open("testdata/nonexistent.torrent")
+		if err == nil {
+			t.Error("Expected error, got nil")
+		} else if !errors.Is(err, os.ErrNotExist) {
+			t.Errorf("Unexpected error: got %v, want %v", err, os.ErrNotExist)
+		}
+	})
 }
