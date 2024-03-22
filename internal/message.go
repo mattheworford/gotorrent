2922fe0d6c7eaa6cc04f60cecdf8cc8b08e3d147
Matthew Orford <mattorford9@gmail.com>
2024-03-21 23:56:44 -0400

    Create message.go

---
+++ internal/message.go
@@ -0,0 +1,82 @@
+package internal
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+)
+
+// MessageType represents the type of message
+type MessageType uint8
+
+const (
+	MsgChoke         MessageType = iota // 0
+	MsgUnchoke                          // 1
+	MsgInterested                       // 2
+	MsgNotInterested                    // 3
+	MsgHave                             // 4
+	MsgBitfield                         // 5
+	MsgRequest                          // 6
+	MsgPiece                            // 7
+	MsgCancel                           // 8
+	MaxPayloadLength = 1 << 14
+)
+
+// PeerMessage stores ID and payload of a message
+type PeerMessage struct {
+	Type    MessageType
+	Payload []byte
+}
+
+// Serialize serializes a message into a buffer of the form
+// <length prefix><message ID><payload>
+// Interprets `nil` as a keep-alive message
+func (m *PeerMessage) Serialize() ([]byte, error) {
+	if m == nil {
+		return make([]byte, 4), nil
+	}
+
+	payloadLength := len(m.Payload)
+	if payloadLength > MaxPayloadLength {
+		return nil, errors.New("payload length exceeds maximum allowed")
+	}
+
+	length := uint32(payloadLength + 1)
+	buf := make([]byte, 4+length)
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(m.Type)
+	copy(buf[5:], m.Payload)
+	return buf, nil
+}
+
+// Read reads a message from a stream
+func Read(r io.Reader) (*PeerMessage, error) {
+	lengthBuf := make([]byte, 4)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	length := binary.BigEndian.Uint32(lengthBuf)
+
+	if length == 0 {
+		return nil, nil
+	}
+
+	if length > MaxPayloadLength {
+		return nil, errors.New("message length exceeds maximum allowed")
+	}
+
+	messageBuf := make([]byte, length)
+	_, err = io.ReadFull(r, messageBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	m := PeerMessage{
+		Type:    MessageType(messageBuf[0]),
+		Payload: messageBuf[1:],
+	}
+
+	return &m, nil
+}
